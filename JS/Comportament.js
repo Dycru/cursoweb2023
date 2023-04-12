b378668d416ad3dd265aee91f5ec40ebdb9187f7
Dycru <114602125+Dycru@users.noreply.github.com>
2023-04-12 16:31:34 +0200

    Examen Practica 1

---
+++ JS/Comportament.js
@@ -0,0 +1,14 @@
+function changeParagraphStyle() {
+    var paragraph = document.getElementById("paragraf");
+    paragraph.style.fontSize = "35px";
+  }
+
+  function changeTitleStyle() {
+    var title = document.getElementById("titol");
+    title.style.color = "red";
+  }
+
+  function changeParagraphContent() {
+    var paragraph = document.getElementById("canvia");
+    paragraph.textContent = "Exercici 5 resolt";
+  }
